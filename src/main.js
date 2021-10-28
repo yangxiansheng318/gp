bb88cd075a1d70a58067cfa2122bb8381822f66c
yxiansen <yxiansen318@163.com>
2021-10-28 17:22:42 +0800

    新增 normalize css, 统一不同浏览器的样式差异

--- src/main.js
+++ src/main.js
@@ -2,5 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 引入 normalize css
+import 'normalize.css'
+import './style/base.scss'
 
 createApp(App).use(store).use(router).mount('#app')
